fc391aa3074525bfe5f180a8dfaef2121fb86373
EchKode <48565771+echkode@users.noreply.github.com>
2024-05-30 00:18:40 +0000

    Add code

---
+++ Functions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+using PhantomBrigade;
+using PhantomBrigade.Data;
+using PhantomBrigade.Functions;
+
+namespace EchKode.PBMods.WaitOnUse.Functions
+{
+	[Serializable]
+	public sealed class CombatActionWaitOnUseValidate : ICombatActionValidationFunction
+	{
+		public bool IsValid(CombatEntity unitCombat) => Helper.FindEquipmentUseAction(unitCombat) != null;
+	}
+
+	[Serializable]
+	public sealed class CombatActionWaitOnUse : ICombatActionExecutionFunction
+	{
+		public void Run(CombatEntity unitCombat, ActionEntity action)
+		{
+			// The wait action has already been created with a start time and the duration from the config file.
+			// We just need to change its duration.
+
+			var equipmentUseAction = Helper.FindEquipmentUseAction(unitCombat, waitAction: action);
+			if (equipmentUseAction == null)
+			{
+				return;
+			}
+
+			var useEndTime = equipmentUseAction.startTime.f + equipmentUseAction.duration.f;
+			var duration = useEndTime - action.startTime.f;
+			if (duration < Helper.MinimumActionDuration)
+			{
+				return;
+			}
+			action.ReplaceDuration(duration);
+		}
+	}
+
+	static class Helper
+	{
+		// Actions under a certain duration cause UI problems.
+		internal const float MinimumActionDuration = 0.3f;
+
+		internal static ActionEntity FindEquipmentUseAction(CombatEntity unitCombat, ActionEntity waitAction = null)
+		{
+			ActionEntity equipmentUseAction = null;
+			var lastMovementTrackActionTime = waitAction == null
+				? ActionUtility.GetLastActionTime(unitCombat, TrackType.Primary)
+				: waitAction.startTime.f;
+			var actions = Contexts.sharedInstance.action.GetEntitiesWithActionOwner(unitCombat.id.id);
+			foreach (var action in actions)
+			{
+				if (action.isDisposed)
+				{
+					continue;
+				}
+				if (!action.hasStartTime || !action.hasDuration)
+				{
+					continue;
+				}
+
+				var actionEndTime = action.startTime.f + action.duration.f;
+				if (actionEndTime < lastMovementTrackActionTime + MinimumActionDuration)
+				{
+					continue;
+				}
+				if (!action.hasActiveEquipmentPart)
+				{
+					continue;
+				}
+
+				if (!action.hasDataLinkActionCore)
+				{
+					continue;
+				}
+
+				var dataCore = action.dataLinkActionCore;
+				if (dataCore.data == null || dataCore.data.trackType != TrackType.Secondary)
+				{
+					continue;
+				}
+
+				var part = IDUtility.GetEquipmentEntity(action.activeEquipmentPart.equipmentID);
+				if (part == null || !part.hasPrimaryActivationSubsystem)
+				{
+					// Weapons have primary activation subsystems.
+					continue;
+				}
+
+				if (part.IsPartTaggedAs(EquipmentTags.defensive))
+				{
+					// Exclude shields since they usually don't need the unit to stand around to be effective.
+					continue;
+				}
+
+				if (equipmentUseAction != null && action.startTime.f > equipmentUseAction.startTime.f)
+				{
+					continue;
+				}
+
+				equipmentUseAction = action;
+			}
+
+			return equipmentUseAction;
+		}
+	}
+}
---
+++ Patch.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+using HarmonyLib;
+
+using PhantomBrigade;
+using PhantomBrigade.Data;
+using PhantomBrigade.Input.Components;
+
+namespace EchKode.PBMods.WaitOnUse
+{
+	[HarmonyPatch]
+	public static partial class Patch
+	{
+		[HarmonyPatch(typeof(CIViewCombatAction), "OnActionSelect")]
+		[HarmonyTranspiler]
+		static IEnumerable<CodeInstruction> Civca_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+			// Fake a PaintingType.Immediate enum value so that an action can bypass timing placement and be put on
+			// the combat timeline immediately.
+			//
+			// Trigger when paintingType == PaintingType.Timing, durationType = DurationType.Variable and duration == 1
+			// This is a unique combo that none of the stock actions has.
+
+			var cm = new CodeMatcher(instructions, generator);
+			var removeSelectedActionMethodInfo = AccessTools.DeclaredMethod(typeof(InputContext), nameof(InputContext.RemoveSelectedAction));
+			var durationTypeFieldInfo = AccessTools.DeclaredField(typeof(DataBlockActionCore), nameof(DataBlockActionCore.durationType));
+			var durationFieldInfo = AccessTools.DeclaredField(typeof(DataBlockActionCore), nameof(DataBlockActionCore.duration));
+			var removeSelectedActionMatch = new CodeMatch(OpCodes.Callvirt, removeSelectedActionMethodInfo);
+			var loadTiming = new CodeInstruction(OpCodes.Ldc_I4_6);
+			var loadZero = new CodeInstruction(OpCodes.Ldc_I4_0);
+			var loadDurationType = new CodeInstruction(OpCodes.Ldfld, durationTypeFieldInfo);
+			var loadDuration = new CodeInstruction(OpCodes.Ldfld, durationFieldInfo);
+			var loadOne = new CodeInstruction(OpCodes.Ldc_R4, 1f);
+			var callPlaceAction = CodeInstruction.Call(typeof(Patch), nameof(PlaceAction));
+			var ret = new CodeInstruction(OpCodes.Ret);
+
+			cm.Start()
+				.MatchEndForward(removeSelectedActionMatch)
+				.Advance(1);
+			var loadAction = cm.Instruction.Clone();
+			cm.Advance(1);
+			var loadDataCore = cm.Instruction.Clone();
+			cm.Advance(3);
+			var loadPaintingType = cm.Instruction.Clone();
+			cm.CreateLabel(out var switchStartLabel);
+			var skipToSwitch = new CodeInstruction(OpCodes.Bne_Un, switchStartLabel);
+
+			cm.InsertAndAdvance(loadPaintingType)
+				.InsertAndAdvance(loadTiming)
+				.InsertAndAdvance(skipToSwitch)
+				.InsertAndAdvance(loadAction)
+				.InsertAndAdvance(loadDataCore)
+				.InsertAndAdvance(loadDurationType)
+				.InsertAndAdvance(loadZero)
+				.InsertAndAdvance(skipToSwitch)
+				.InsertAndAdvance(loadAction)
+				.InsertAndAdvance(loadDataCore)
+				.InsertAndAdvance(loadDuration)
+				.InsertAndAdvance(loadOne)
+				.InsertAndAdvance(skipToSwitch)
+				.InsertAndAdvance(loadAction)
+				.InsertAndAdvance(callPlaceAction)
+				.InsertAndAdvance(ret);
+
+			return cm.InstructionEnumeration();
+		}
+
+		public static void PlaceAction(DataContainerAction actionData)
+		{
+			var input = Contexts.sharedInstance.input;
+			var combat = Contexts.sharedInstance.combat;
+			var unitCombat = IDUtility.GetCombatEntity(combat.unitSelected.id);
+			var startTime = ActionUtility.GetLastActionTime(unitCombat, TrackType.Primary) + 0.001f;
+			combat.ReplacePredictionTimeTarget(startTime);
+			input.ReplaceCombatUIMode(CombatUIModes.Time_Placement);
+			input.ReplaceSelectedAction(actionData.key);
+			AudioUtility.CreateAudioEvent("ui_unit_select_action");
+			CombatUIUtility.AttemptToFinishTimePlacement();
+		}
+	}
+}
